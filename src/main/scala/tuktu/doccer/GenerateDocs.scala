130472181fe0c102c2cea0ef1a4e1d18c4873090
NTPape <NTPape@users.noreply.github.com>
2015-06-23 12:13:39 +0200

    Add default values to documentation; use BufferedWriter.newLine for writing newlines

--- src/main/scala/tuktu/doccer/GenerateDocs.scala
+++ src/main/scala/tuktu/doccer/GenerateDocs.scala
@@ -5,6 +5,7 @@ import java.io.File
 import scala.io.Codec.string2codec
 import scala.io.Source
 import play.api.libs.json.JsObject
+import play.api.libs.json.JsValue
 import play.api.libs.json.Json
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -15,38 +16,49 @@ object GenerateDocs {
     /**
      * Reads a single parameter and recurses for the others
      */
-    def readParameter(json: JsObject, writer: BufferedWriter, nestDepth: Int): Unit =  {
+    def readParameter(json: JsObject, writer: BufferedWriter, nestDepth: Int): Unit = {
         // Get fields we need
-        val name = {
-            val n = (json \ "name").as[String]
-            if (n == "") "[UNNAMED]"
-            else n
+        val name = (json \ "name").asOpt[String] match {
+            case None | Some("") => "[UNNAMED]"
+            case Some(s) => s
         }
         val dataType = (json \ "type").as[String]
         val required = (json \ "required").asOpt[Boolean]
+        val default = (json \ "default").asOpt[JsValue] match {
+            case None => ""
+            case Some(dflt) => ", default = " + dflt.toString
+        }
         val parameters = (json \ "parameters").asOpt[List[JsObject]]
         val description = (json \ "description").asOpt[String]
-        
+
         // Generate output and recurse for all parameters
         writer.write(
-                {(for (i <- 1 to nestDepth * 2) yield " ").mkString("")} +
-                "* **" + name + "** *(type: " + dataType + ")* `[" + {if (required.getOrElse(false)) "Required" else "Optional"} + "]`\r\n" +
-                {description match {
-                    case None => "\r\n"
-                    case Some(descr) => {
-                        (for (i <- 1 to nestDepth * 2) yield " ").mkString("") +
-                        "- " + description.getOrElse("No description present.") + "\r\n \r\n"
-                    }
-                }}
-        )
-        
+            (for (i <- 1 to nestDepth * 2) yield " ").mkString("") +
+                "* **" + name + "** *(type: " + dataType + ")* `[" + {
+                    if (required.getOrElse(false))
+                        "Required"
+                    else
+                        "Optional" + default
+                } +
+                "]`")
+
+        writer.newLine
+        description match {
+            case None => {}
+            case Some(descr) => {
+                writer.write((for (i <- 1 to nestDepth * 2) yield " ").mkString("") + "- " + descr)
+                writer.newLine
+            }
+        }
+        writer.newLine
+
         // Recurse
         parameters match {
             case None => {}
             case Some(params) => params.foreach(parameter => readParameter(parameter, writer, nestDepth + 1))
         }
     }
-    
+
     /**
      * Bootstraps a documentation
      */
@@ -56,32 +68,37 @@ object GenerateDocs {
         val className = (json \ "class").as[String]
         val description = (json \ "description").asOpt[String]
         val parameters = (json \ "parameters").asOpt[List[JsObject]]
-        
+
         // Write out main output
-        writer.write(
-                "### " + className + "\r\n" +
-                description.getOrElse("No description present.") + "\r\n\r\n"
-        )
-        
+        writer.write("### " + className)
+        writer.newLine
+        writer.write(description.getOrElse("No description present."))
+        writer.newLine
+        writer.newLine
+
         // Process all parameters
         parameters match {
-            case None => writer.write("No parameters.\r\n\r\n")
+            case None => {
+                writer.write("No parameters.")
+                writer.newLine
+                writer.newLine
+            }
             case Some(params) => params.foreach(parameter => readParameter(parameter, writer, 1))
         }
     }
-        
+
     /**
      * Generates a doc for a single file
      */
     def generateDoc(file: File, outputFolder: String, typeName: String) = {
         println("Processing file " + file.getAbsoluteFile)
-        
+
         // Open file for reading
         val content = Source.fromFile(file)("utf-8").getLines.mkString
-    
+
         // Parse the JSON
         val json = Json.parse(content).asInstanceOf[JsObject]
-        
+
         // Create dirs
         val fileName = file.getAbsolutePath
         val outputFile = new File(outputFolder + "/" + fileName.drop(fileName.indexOf(typeName) + typeName.size + 1) + ".md")
@@ -91,7 +108,7 @@ object GenerateDocs {
         readTransformer(json, writer)
         writer.close
     }
-    
+
     /**
      * Generates documentation for all files
      */
@@ -106,7 +123,7 @@ object GenerateDocs {
             }
         })
     }
-    
+
     def main(args: Array[String]) {
         if (args.size != 2) {
             println("Not enough arguments found!")
@@ -116,16 +133,16 @@ object GenerateDocs {
             println("  documentation output folder")
             sys.exit(0)
         }
-        
+
         // Get args
         val metaFolder = args(0)
         val outputFolder = args(1)
-        
+
         // Remove the output folder first
         val out = new File(outputFolder)
         out.delete
         out.mkdir
-        
+
         // Bootstrap generators and processors
         val generatorsFolder = new File(metaFolder + "/generators")
         generateAllDocs(generatorsFolder, outputFolder + "/generators/", "generators")
